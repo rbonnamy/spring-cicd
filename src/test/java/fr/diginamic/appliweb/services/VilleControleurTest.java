c097aa2389004541c7126c40776b621807a5828e
rbonnamy <rbonnamy@diginamic.fr>
2026-06-01 15:49:47 +0200

    Correction TU

--- src/test/java/fr/diginamic/appliweb/services/VilleControleurTest.java
+++ src/test/java/fr/diginamic/appliweb/services/VilleControleurTest.java
@@ -8,11 +8,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -27,10 +25,6 @@ public class VilleControleurTest {
     
     @Mock
     private DepartementService departementService;
-    
-    public void init() {
-    	//Mockito.when(departementService.completeDepartement(Mockito.any())).doNothing();
-    }
 
     @Test
     public void testExtraire(){
